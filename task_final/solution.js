cc15130d574253ec8ad4ec7ccbede75177e9d2eb
Ilya Selkin <ilya_selkin@mail.ru>
2021-11-12 21:17:05 +0300

    Final Task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let all_data = {
+        "data": {
+            "client": "",
+            "order": {
+                "address":"",
+                "sum": 0,
+            },
+            "goods": []
+        }
+    };
 
-    let countOfGoods = goods.length;
+    all_data.data.client = `${name} ${phone}`;
+    all_data.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    all_data.data.order.sum = sum;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < goods.length; i++) {
+        all_data.data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    return JSON.stringify(all_data);
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
 }
